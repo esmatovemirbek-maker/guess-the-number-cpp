8b8f76d65794f4b9b97d495bddb005ba40219e4b
esmatovemirbek-maker <esmatovemirbek@gmail.com>
2025-12-16 20:21:18 +0300

    Add guess the number game in C++

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <random>
+
+int main() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(1, 100);
+
+    int secret = dist(gen);
+    int guess = 0;
+
+    std::cout << "Guess the number (1–100): ";
+
+    while (guess != secret) {
+        std::cin >> guess;
+        if (guess < secret) std::cout << "Higher!\n";
+        if (guess > secret) std::cout << "Lower!\n";
+    }
+
+    std::cout << "You win!\n";
+}
